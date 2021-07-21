68cb209267244a98f43080d1383ac507bf46d191
will_halloward <willhalloward@gmail.com>
2021-07-22 00:07:53 +0200

    updated main

--- main.py
+++ main.py
@@ -11,12 +11,6 @@ from discord.ext import commands, tasks
 
 import secrets
 
-intents = discord.Intents.none()
-intents.members = True
-intents.emojis = True
-intents.guild_messages = True
-intents.guild_reactions = True
-
 context = ssl.SSLContext(ssl.PROTOCOL_SSLv23)
 home = os.path.expanduser('~')
 context.load_verify_locations(f"{home}/ssl-cert/server-ca.pem")
@@ -25,12 +19,13 @@ logging.basicConfig(filename=f'{home}/twi_bot_shard/cognita.log',
                     format='%(asctime)s :: %(levelname)-8s :: %(filename)s :: %(message)s',
                     level=logging.INFO)
 logging.info("Cognita starting")
-
+intents = discord.Intents.default()  # All but the two privileged ones
+intents.members = True  # Subscribe to the Members intent
 bot = commands.Bot(
     command_prefix='!',
     description="The Wandering Inn helper",
     case_insensitive=True,
-    intent=intents)
+    intents=intents)
 
 cogs = ['cogs.gallery', 'cogs.links_tags', 'cogs.patreon_poll', 'cogs.twi', 'cogs.owner', 'cogs.other', 'cogs.mods',
         'cogs.stats']
@@ -54,7 +49,6 @@ async def on_ready():
     bot.remove_listener(stats_cog.reaction_add, name="on_raw_reaction_add")
     bot.remove_listener(stats_cog.reaction_remove, name="on_raw_reaction_remove")
     status_loop.start()
-    print(f'Logged in as: {bot.user.name}\nVersion: {discord.__version__}\n')
     logging.info(f'Logged in as: {bot.user.name}\nVersion: {discord.__version__}\n')
 
 
@@ -75,10 +69,11 @@ status = cycle(["Killing the mages of Wistram",
                 "Keeping secrets",
                 "Hiding corpses",
                 "Mending Pirateaba's broken hands",
-                "Longing for Zelkyr",
+                "Longing for answers",
                 "Hoarding knowledge",
                 "Dusting off priceless artifacts",
-                "Praying for Mating Rituals 4"])
+                "Praying for Mating Rituals 4",
+                "Plotting demise of nosy half-elfs"])
 
 
 @tasks.loop(seconds=10)
@@ -86,31 +81,31 @@ async def status_loop():
     await bot.change_presence(activity=discord.Game(next(status)))
 
 
-# @bot.event
-# async def on_command_error(ctx, error):
-#     logging.warning(f"{type(error).__name__} - {error}")
-#     if hasattr(ctx.command, "on_error"):
-#         return
-#     elif isinstance(error, commands.MissingRequiredArgument):
-#         await ctx.send(error)
-#     elif isinstance(error, commands.NotOwner):
-#         await ctx.send(f"Sorry {ctx.author.display_name} only ~~Zelkyr~~ Sara may do that.")
-#     elif isinstance(error, commands.MissingRole):
-#         await ctx.send("I'm sorry, you don't seem to have the required role for that")
-#     elif isinstance(error, commands.CommandOnCooldown):
-#         await ctx.send(f"That command is on cooldown, please wait a bit.")
-#     elif isinstance(error, commands.BadArgument):
-#         await ctx.send(f"Error: {error}")
-#     elif isinstance(error, discord.NotFound):
-#         await ctx.send(f"I could not find that message - {error}")
-#     elif isinstance(error, discord.Forbidden):
-#         await ctx.send(f"Error: I don't have the right permissions for that. - {error}")
+@bot.event
+async def on_command_error(ctx, error):
+    logging.warning(f"{type(error).__name__} - {error}")
+    if hasattr(ctx.command, "on_error"):
+        return
+    elif isinstance(error, commands.MissingRequiredArgument):
+        await ctx.send(error)
+    elif isinstance(error, commands.NotOwner):
+        await ctx.send(f"Sorry {ctx.author.display_name} only ~~Zelkyr~~ Sara may do that.")
+    elif isinstance(error, commands.MissingRole):
+        await ctx.send("I'm sorry, you don't seem to have the required role for that")
+    elif isinstance(error, commands.CommandOnCooldown):
+        await ctx.send(f"That command is on cooldown, please wait a bit.")
+    elif isinstance(error, commands.BadArgument):
+        await ctx.send(f"Error: {error}")
+    elif isinstance(error, discord.NotFound):
+        await ctx.send(f"I could not find that message - {error}")
+    elif isinstance(error, discord.Forbidden):
+        await ctx.send(f"Error: I don't have the right permissions for that. - {error}")
 
 
 @bot.event
 async def on_command(ctx):
     logging.info(
-        f"{ctx.author.name} invoked {ctx.command} with arugments {ctx.kwargs} in channel {ctx.channel.name} from guild {ctx.guild.name}")
+        f"{ctx.author.name} invoked {ctx.command} with arguments {ctx.kwargs} in channel {ctx.channel.name} from guild {ctx.guild.name}")
 
 
 bot.loop.run_until_complete(create_db_pool())
